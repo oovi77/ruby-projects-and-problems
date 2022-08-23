3f5c8b23578f3a31e2ddc2a832671bcd21036ebb
oovi77 <oovi77@gmail.com>
2022-08-23 12:12:03 -0400

    completed Ruby Substrings problem

--- sub_strings/sub_strings.rb
+++ sub_strings/sub_strings.rb
@@ -21,11 +21,12 @@ that was found in the original string and how many times it was found.
 =end
 
 #"string".split 
+=begin
 temp = "Howdy partner, sit down! How's it going?"
 p temp.split # ["Howdy", "partner,", "sit", "down!", "How's", "it", "going?"]
 t1 = "howdy"
 p t1.split
-
+=end
 
 #1. loop through each subtring in dictionary
 #2. for each substring in dictionary, loop through each word in the sentance to see if it is in there
@@ -33,7 +34,39 @@ p t1.split
 
 #use include? to check if a match happens? look into that one hash example with votes...
 
-def substrings(word, dictionary)
+def substrings(sentence, dictionary)
 
+    result = Hash.new(0)
+    sentence = sentence.split
+    sentence = sentence.map {|element| element.downcase}
+    p sentence
+
+    dictionary.each do |string|
+        sentence.each do |word|
+            if (word.include?(string))
+                result[string] += 1
+            end
+        end
+    end
+
+    return result
 end
 
+
+#need to use map or something to make each word in sentence lowercase b/c include? is case sensitive
+
+#s.downcase
+
+dictionary1 = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+p substrings("BeLow", dictionary1)   #this works { "below" => 1, "low" => 1 }
+p substrings("Howdy partner, sit down! How's it going?", dictionary1)
+#temp = "foo"
+#p temp.include?("f")
+#p temp.include?("F")
+
+=begin
+t1 = Hash.new(0)
+t1["i"] += 1
+p t1
+=end
